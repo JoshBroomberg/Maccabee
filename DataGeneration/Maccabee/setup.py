087329b1c906b73b89d483965882cc0f4aa83cf6
Josh Broomberg <joshbroomberg@gmail.com>
2020-01-25 15:58:17 +0800

    bumping package version

--- DataGeneration/Maccabee/setup.py
+++ DataGeneration/Maccabee/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='maccabee',
-     version='0.0.11',
+     version='0.0.12',
      scripts=[],
      author="Josh Broomberg",
      author_email="joshbroomberg@gmail.com",
