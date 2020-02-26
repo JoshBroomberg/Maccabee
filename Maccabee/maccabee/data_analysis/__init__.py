7c380c455da7b8077c7bd5aa6f6537dc816ddad0
Josh Broomberg <joshbroomberg@gmail.com>
2020-02-26 10:52:24 -0800

    small updates to docs

--- Maccabee/maccabee/data_analysis/__init__.py
+++ Maccabee/maccabee/data_analysis/__init__.py
@@ -1,6 +1,8 @@
-"""The data analysis module contains the code responsible for calculating :term:`data metrics <data metric>` - metrics which quantify the location of a data set in the :term:`distributional problem space`. The module is not responsible for actually executing these calculations, that is handled by the :mod:`maccabee.benchmarking` module. Rather, this module is responsible for defining the actual metrics used to quantify the location of a data set on each :term:`distributional problem space axis`.
+"""The data analysis module contains the code responsible for calculating :term:`data metrics <data metric>` - metrics which quantify the location of a data set in the :term:`distributional problem space`. More on the theory behind these metrics can be found in Chapter 3 of the :download:`theory paper </maccabee-theory-paper.pdf>`.
 
-This module is split into two submodules. :mod:`~maccabee.data_analysis.data_metrics` contains the metric definitions and :mod:`~maccabee.data_analysis.data_analysis` contains the code which calculates these metrics given a :class:`maccabee.data_generation.generated_data_set.GeneratedDataSet` instance and  code which plots the calculated metric results.
+The module is not responsible for actually executing these calculations, that is handled by the :mod:`maccabee.benchmarking` module. Rather, this module is responsible for defining the actual metrics used to quantify the location of a data set on each :term:`distributional problem space axis` and providing wrapper functionality to calculate multiple metrics given a :class:`~maccabee.data_generation.generated_data_set.GeneratedDataSet` instance.
+
+This module is split into two submodules. :mod:`~maccabee.data_analysis.data_metrics` contains the metric definitions and :mod:`~maccabee.data_analysis.data_analysis` contains the code which calculates these metrics given a :class:`maccabee.data_generation.generated_data_set.GeneratedDataSet` instance and code which plots calculated metric results.
 
 .. note::
 
